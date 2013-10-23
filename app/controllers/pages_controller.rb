270cc341d961c9ea06f2b9f6567e3c4bf02f2528
comebacknader <comebacknader@hotmail.com>
2013-10-23 13:37:45 -0700

    Added analytics and different ordering to Home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
 	skip_before_filter :authorize
 
 	def home
-		@products = Product.order("created_at ASC")
+		@products = Product.order("created_at DESC")
 	end
 
 	def men
